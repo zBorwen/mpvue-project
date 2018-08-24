faf0cbdf7328b8c89023de09427f72ca228ebe0c
zBorwen <136676959@qq.com>
2018-08-24 17:18:45 +0800

    个人中心

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import {showSuccess} from 'common/until';
 
 export default {
   created() {
-    const user = wx.getStorageSync('useInfo');
+    const user = wx.getStorageSync('userInfo');
     if (!user) {
       qcloud.setLoginUrl(config.loginUrl);
       qcloud.login({
         success(userInfo) {
           showSuccess('登陆成功');
-          wx.setStorageSync('useInfo', userInfo);
-          console.log('登录成功', userInfo);
+          wx.setStorageSync('userInfo', userInfo);
+          // console.log('登录成功', userInfo);
         },
         fail(err) {
           console.log('登录失败', err);
--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -1,17 +1,47 @@
 <template>
-  <div>
-    个人中心
+  <div class="container">
+    <div class="userinfo">
+      <img :src="userInfo.avatarUrl" alt="">
+      <p>{{userInfo.nickName}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userInfo: {}
+    };
+  },
   created() {
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.userInfo = userInfo;
+    } else {
+      this.userInfo = {nickName: '路人甲', avatarUrl: ''};
+    }
   },
   methods: {
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+@function rpx($value) {
+  @return $value * 1rpx;
+}
+.container{
+  padding: 0 rpx(30);
+  .userinfo{
+    margin-top: rpx(100);
+    text-align: center;
+    img{
+      width: rpx(150);
+      height: rpx(150);
+      margin: rpx(20);
+      border-radius: 50%;
+    }
+  }
+}
 </style>
